1e7ee4c2d3636e72852b50109585c80e97529968
Sefa Kose <sfkse5591@gmail.com>
2021-09-05 23:15:39 +0200

    Implement SPA part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const RenderMenuItem = ({ dish, onClick }) => {
 
     return (
-        <Card onClick={() => onClick(dish.id)}>
+        <Card >
+            <Link to={`/menu/${dish.id}`}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <p>{dish.description}</p>
+                </CardImgOverlay>
+            </Link>
 
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-                <p>{dish.description}</p>
-            </CardImgOverlay>
         </Card>
     )
 }
@@ -20,13 +21,25 @@ const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
         return (
             <div id={dish.id} className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish} />
             </div>
         )
     });
     return (
 
         <div className='container' >
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to="/home">Home</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>
+            </div>
             <div className="row">
                 {menu}
             </div>
